bbdb75cd409d5edb1cc430582bb586d37588f5bd
obrm <obrm770@gmail.com>
2021-05-08 15:40:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Markdown preview
+
+Markdown preview app built with React.
+Built as part of a [React course](https://www.udemy.com/course/react-tutorial-and-projects-course).
+
+[Used react-markdown library](https://www.npmjs.com/package/react-markdown)
+
+[Visit project site](https://obrm-markdown-preview.netlify.app)
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const App = () => {
+  const text = `
+  ## markdown preview
+  # Give it a try!
+  ### here are some examples:
+  
+  > Here is a quote by someone famous  
+  #### Heading four  
+  Lorem ipsum **BOLD** sit amet consectetur adipisicing elit.
+  
+  [here is a link to Google](https://www.google.com)
+
+  ![image](https://images.pexels.com/photos/7438529/pexels-photo-7438529.jpeg)
+
+  \`\`\`
+  console.log('Here is a block of code!')
+  \`\`\`
+  `
+  const [markdown, setMarkdown] = useState(text)
+
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  )
+}
+
+export default App
